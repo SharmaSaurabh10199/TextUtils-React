391eb34849de557136d5a85b980772f49339cbf1
Saurabh Sharma <sharmasaurabh10199@gmail.com>
2022-08-19 10:48:06 +0530

    build folder created

--- src/Components/TextArea.js
+++ src/Components/TextArea.js
@@ -4,9 +4,11 @@ export default function TextArea({ mode, showAlert }) {
   // using the states to change the values
   const [text, setText] = useState("");
 
+  const [result, setResult] = useState("");
+
   // setting text to uppercase
   function toUpperCase() {
-    setText(text.toUpperCase());
+    setResult(text.toUpperCase());
     showAlert("converted to upper case", "success");
   }
 
@@ -17,39 +19,62 @@ export default function TextArea({ mode, showAlert }) {
 
   // to change to lowercae
   function toLoweCase() {
-    setText(text.toLowerCase());
+    setResult(text.toLowerCase());
     showAlert("converted to lower case", "success");
   }
 
   // to clear the text
   function clear() {
-    setText("");
+    setResult("");
+
     showAlert("cleared all the text", "success");
   }
 
   // to remove the extra space
   function format() {
     let txt = text.split(/[ ]+/);
-    setText(txt.join(" "));
+    setResult(txt.join(" "));
     showAlert("formating done", "success");
+  }
+
+  // to count the text:
+
+  function textCount() {
+    let obj = {};
+    let Words = text.split(" ").filter(function (n) {
+      return n != " ";
+    }).length;
+    let characters = text.length;
+    let lines = text.split("\n").length;
+    // why its not coming to the new line
+    let summary = `lines: ${lines}  ,   words: ${Words} , characters: ${characters}`;
+    setResult(summary);
+    console.log(lines);
+  }
+
+  // function to reverse string:
+  function reverse() {
+    setResult(text.split("").reverse().join(""));
   }
 
   return (
     <div
       className={`container my-5 text-${mode == "light" ? "black" : "white"}`}
     >
-      <h2>Enter text here </h2>
+      <h1>Enter text here </h1>
       <div className="form-floating">
         <textarea
           style={{
             backgroundColor: mode === "light" ? "white" : "grey",
             color: mode === "light" ? "black" : "white",
+            height: "180px",
           }}
           className="form-control"
           placeholder="Leave a comment here"
           id="floatingTextarea"
           value={text}
           onChange={handleOnChange}
+          rows="99"
         ></textarea>
       </div>
       <div className="container my-3 ">
@@ -62,31 +87,56 @@ export default function TextArea({ mode, showAlert }) {
         </button>
         <button
           type="button"
-          className="  btn btn-primary mx-3"
+          className="  btn btn-primary mx-3 my-1"
           onClick={toLoweCase}
         >
           to lowerCase
         </button>
         <button
           type="button"
-          className="  btn btn-primary mx-3"
+          className="  btn btn-primary mx-3 my-1"
           onClick={clear}
         >
           clear text
         </button>
         <button
           type="button"
-          className="  btn btn-primary mx-3"
+          className="  btn btn-primary mx-3 my-1"
           onClick={format}
         >
           format
         </button>
+        <button
+          type="button"
+          className="  btn btn-primary mx-3 my-1"
+          onClick={textCount}
+        >
+          Text Counter
+        </button>
+        <button
+          type="button"
+          className="  btn btn-primary mx-3 my-1"
+          onClick={reverse}
+        >
+          Reverse String
+        </button>
       </div>
-
-      <div className="contaier my-4">
-        <h1>Your text summary</h1>
-        <p>words: {text.split(" ").length - 1}</p>
-        <p>characters : {text.length}</p>
+      {/* the output section */}
+      <div>
+        <h2>Your Results</h2>
+        <div
+          className="contaier my-4"
+          style={{
+            border: "1px solid #cac3c3",
+            borderRadius: "5px",
+            padding: "1rem 0.75rem",
+            height: "100px",
+          }}
+        >
+          {/* <p>words: {text.split(/[ ]+/).length - 1}</p>
+        <p>characters : {text.length}</p> */}
+          <p>{result}</p>
+        </div>
       </div>
     </div>
   );
